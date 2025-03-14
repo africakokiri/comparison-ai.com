ee51ba7952fd2232fb9b196a6f666e344ad08901
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 12:41:04 +0900

    Prompt 컴포넌트 배치

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -14,35 +14,40 @@ export const Prompt = () => {
   };
 
   return (
-    <form onClick={handleButtonClick}>
-      <div
-        className="flex w-1/2 items-center justify-between rounded-md
-border-[1px] border-black pr-2"
+    <div className="bg- flex w-full justify-center">
+      <form
+        onClick={handleButtonClick}
+        className="w-1/2"
       >
-        <label
-          htmlFor="prompt-textarea"
-          className="sr-only"
+        <div
+          className="flex w-full items-center justify-between rounded-md
+border-[1px] border-black pr-2"
         >
-          Enter the prompt
-        </label>
-        <TextareaAutosize
-          ref={textareaRef}
-          id="prompt-textarea"
-          name="textarea"
-          minRows={2}
-          maxRows={5}
-          className="w-full resize-none rounded-md px-2 py-1.5
+          <label
+            htmlFor="prompt-textarea"
+            className="sr-only"
+          >
+            Enter the prompt
+          </label>
+          <TextareaAutosize
+            ref={textareaRef}
+            id="prompt-textarea"
+            name="textarea"
+            minRows={2}
+            maxRows={5}
+            className="w-full resize-none rounded-md px-2 py-1.5
 outline-none"
-          placeholder="Enter the prompt..."
-        />
-        <button
-          type="button"
-          className="flex h-[36px] w-[36px] items-center justify-center
+            placeholder="Enter the prompt..."
+          />
+          <button
+            type="button"
+            className="flex h-[36px] w-[36px] items-center justify-center
 rounded-full bg-black text-white"
-        >
-          <ChevronUp />
-        </button>
-      </div>
-    </form>
+          >
+            <ChevronUp />
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
