c99ef822fb8139540fd2629a3da130a3b4db3646
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 13:06:24 +0900

    Outputs 컴포넌트 기본 레이아웃 배치

--- src/components/Outputs.tsx
+++ src/components/Outputs.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { ReactNode } from "react";
+
+const ResponseBox = ({
+  children,
+  modelName
+}: {
+  readonly children: ReactNode;
+  modelName: string;
+}) => {
+  return (
+    <div className="w-1/3">
+      <h2>{modelName}</h2>
+      {children}
+    </div>
+  );
+};
+
 export const Outputs = () => {
   return (
     <div className="flex h-full w-full justify-between gap-8">
-      <div className="w-1/3 bg-red-100"></div>
-      <div className="w-1/3 bg-yellow-100">B</div>
-      <div className="w-1/3 bg-green-100">C</div>
+      <ResponseBox modelName="Chat-GPT">A</ResponseBox>
+      <ResponseBox modelName="Gemini">B</ResponseBox>
+      <ResponseBox modelName="Claude">C</ResponseBox>
     </div>
   );
 };
