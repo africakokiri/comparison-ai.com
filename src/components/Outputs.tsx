3b4878c88860afb25c9cd8572533ac507b8db8ef
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 13:16:37 +0900

    Ai Model 이미지 추가

--- src/components/Outputs.tsx
+++ src/components/Outputs.tsx
@@ -1,17 +1,28 @@
 "use client";
 
+import Image from "next/image";
 import { ReactNode } from "react";
 
 const ResponseBox = ({
   children,
-  modelName
+  modelName,
+  imgName
 }: {
   readonly children: ReactNode;
   modelName: string;
+  imgName: string;
 }) => {
   return (
     <div className="w-1/3">
-      <h2>{modelName}</h2>
+      <div className="flex items-center gap-2">
+        <Image
+          src={`/ai-models/${imgName}.svg`}
+          alt=""
+          height={24}
+          width={24}
+        />
+        <h2 className="text-xl">{modelName}</h2>
+      </div>
       {children}
     </div>
   );
@@ -20,9 +31,24 @@ const ResponseBox = ({
 export const Outputs = () => {
   return (
     <div className="flex h-full w-full justify-between gap-8">
-      <ResponseBox modelName="Chat-GPT">A</ResponseBox>
-      <ResponseBox modelName="Gemini">B</ResponseBox>
-      <ResponseBox modelName="Claude">C</ResponseBox>
+      <ResponseBox
+        modelName="gpt-4o-mini"
+        imgName="gpt"
+      >
+        A
+      </ResponseBox>
+      <ResponseBox
+        modelName="Gemini"
+        imgName="gemini"
+      >
+        B
+      </ResponseBox>
+      <ResponseBox
+        modelName="Claude"
+        imgName="claude"
+      >
+        C
+      </ResponseBox>
     </div>
   );
 };
