f0efe8a58a3c2f79a41a8d533f60b3e3f83016b6
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 12:39:57 +0900

    Prompt 컴포넌트 적용

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,11 +1,48 @@
 "use client";
 
+import { ChevronUp } from "lucide-react";
+import { useRef } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 export const Prompt = () => {
+  const textareaRef = useRef(null);
+
+  const handleButtonClick = () => {
+    if (textareaRef.current) {
+      (textareaRef.current as HTMLTextAreaElement).focus();
+    }
+  };
+
   return (
-    <div className="bg-blue-200">
-      <TextareaAutosize />
-    </div>
+    <form onClick={handleButtonClick}>
+      <div
+        className="flex w-1/2 items-center justify-between rounded-md
+border-[1px] border-black pr-2"
+      >
+        <label
+          htmlFor="prompt-textarea"
+          className="sr-only"
+        >
+          Enter the prompt
+        </label>
+        <TextareaAutosize
+          ref={textareaRef}
+          id="prompt-textarea"
+          name="textarea"
+          minRows={2}
+          maxRows={5}
+          className="w-full resize-none rounded-md px-2 py-1.5
+outline-none"
+          placeholder="Enter the prompt..."
+        />
+        <button
+          type="button"
+          className="flex h-[36px] w-[36px] items-center justify-center
+rounded-full bg-black text-white"
+        >
+          <ChevronUp />
+        </button>
+      </div>
+    </form>
   );
 };
