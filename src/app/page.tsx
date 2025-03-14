5c5f3d370ff85e6340ea2335c75cb857a341922d
Choi Chan Hwan <africakokiri@gmail.com>
2025-03-14 13:19:10 +0900

    Header(Logo) 컴포넌트 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Header } from "@/components/Header";
 import { Outputs } from "@/components/Outputs";
 import { Prompt } from "@/components/Prompt";
 
 export default function page() {
   return (
     <div className="h-full w-full space-y-4 p-4">
-      <Header />
       <Outputs />
       <Prompt />
     </div>
