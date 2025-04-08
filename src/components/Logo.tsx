762700366c53eb7f0d8263c161e5b49611a8033b
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-08 14:06:30 +0900

    Logo 컴포넌트 위치 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const robotoMonoVar = Roboto_Mono({ subsets: ["latin"] });
 
 export const Logo = () => {
   return (
-    <footer className="fixed bottom-8 right-8">
+    <footer className="fixed bottom-12 right-8">
       <h1 className={`${robotoMonoVar.className} text-2xl font-light`}>
         Comparison-ai.com
       </h1>
