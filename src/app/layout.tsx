0b259c46880d6448923a4b8c7334dc420bd34cbb
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-08 14:20:47 +0900

    Lang ko => en 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="en">
       <body className="antialiased">
         <NextThemesProvider>{children}</NextThemesProvider>
       </body>
--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -2,13 +2,13 @@
 
 import { ChevronUp } from "lucide-react";
 import { useRef } from "react";
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 import TextareaAutosize from "react-textarea-autosize";
 
 export const PromptInput = () => {
-  const isDesktop = useMediaQuery({
-    query: "(min-width: 769px)"
-  });
+  // const isDesktop = useMediaQuery({
+  //   query: "(min-width: 769px)"
+  // });
 
   const textareaRef = useRef(null);
 
@@ -20,29 +20,27 @@ export const PromptInput = () => {
     (textareaRef.current as HTMLFormElement).focus();
   };
 
-  if (isDesktop) {
-    return (
-      <form
-        action=""
-        onClick={handleFormOnclick}
-        className="fixed bottom-8 left-[25%] flex w-1/2 items-center
+  return (
+    <form
+      action=""
+      onClick={handleFormOnclick}
+      className="fixed bottom-8 left-[25%] flex w-1/2 items-center
 rounded-md border-[1px] border-black pr-3 shadow-md transition-shadow
 focus-within:shadow-xl hover:shadow-xl"
-      >
-        <TextareaAutosize
-          minRows={2}
-          maxRows={8}
-          ref={textareaRef}
-          className="w-full resize-none rounded-md px-3 py-1.5
-outline-none"
-        />
-        <button
-          className="flex h-8 w-8 items-center justify-center rounded-full
+    >
+      <TextareaAutosize
+        minRows={2}
+        maxRows={8}
+        placeholder="Prompt..."
+        ref={textareaRef}
+        className="w-full resize-none rounded-md px-3 py-1.5 outline-none"
+      />
+      <button
+        className="flex h-8 w-8 items-center justify-center rounded-full
 bg-black text-white"
-        >
-          <ChevronUp />
-        </button>
-      </form>
-    );
-  }
+      >
+        <ChevronUp />
+      </button>
+    </form>
+  );
 };
