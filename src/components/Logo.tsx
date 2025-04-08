6a70491d52f15286a6cf76a40eca1d748faa3494
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-08 14:18:28 +0900

    Logo 컴포넌트 모바일에서는 보이지 않도록 수정

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ const robotoMonoVar = Roboto_Mono({ subsets: ["latin"] });
 
 export const Logo = () => {
   return (
-    <footer className="fixed bottom-12 right-8">
+    <footer className="bottom-12 right-8 hidden md:fixed md:block">
       <h1 className={`${robotoMonoVar.className} text-2xl font-light`}>
         Comparison-ai.com
       </h1>
