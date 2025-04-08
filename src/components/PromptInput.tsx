572a8841238781c49df402103e4fa1832d32c22a
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-08 14:05:42 +0900

    PromptInput 컴포넌트에서 외곽 영역을 클릭해도 focus 되도록 설정

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { ChevronUp } from "lucide-react";
+import { useRef } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 export const PromptInput = () => {
+  const textareaRef = useRef(null);
+
+  const handleFormOnclick = () => {
+    if (!textareaRef.current) {
+      return null;
+    }
+
+    (textareaRef.current as HTMLFormElement).focus();
+  };
+
   return (
     <form
       action=""
+      onClick={handleFormOnclick}
       className="fixed bottom-8 left-[25%] flex w-1/2 items-center
 rounded-md border-[1px] border-black pr-3 shadow-md transition-shadow
 focus-within:shadow-xl hover:shadow-xl"
@@ -14,6 +26,7 @@ focus-within:shadow-xl hover:shadow-xl"
       <TextareaAutosize
         minRows={2}
         maxRows={8}
+        ref={textareaRef}
         className="w-full resize-none rounded-md px-3 py-1.5 outline-none"
       />
       <button
