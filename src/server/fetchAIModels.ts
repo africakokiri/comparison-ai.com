6cfa14ef869ef7d4608f7c03370fb4c01b23c6bf
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-08 14:51:44 +0900

    Gemini 연결 함수 추가

--- src/server/fetchAIModels.ts
+++ src/server/fetchAIModels.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import OpenAI from "openai";
 
 export const FETCH_GPT = async (prompt: string) => {
@@ -13,4 +14,16 @@ export const FETCH_GPT = async (prompt: string) => {
   });
 
   return response.output_text;
+};
+
+export const FETCH_GEMINI = async (prompt: string) => {
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.5-pro-exp-03-25"
+  });
+
+  const result = await model.generateContent([prompt]);
+
+  return result.response.text();
 };
